7742a34bfaefc34edbe40280fbf13cd5aec4eba4
ApoloJ <mjlozanoh98@gmail.com>
2021-01-18 14:04:56 -0500

    monsters images and email added

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './card.styles.css'
 
 const Card = (props) => {
-    return ( <div className='card-container'><h1>{props.monster.name}</h1></div> );
+    return ( 
+    
+    <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster"/>
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+    </div> );
 }
  
 export default Card;
