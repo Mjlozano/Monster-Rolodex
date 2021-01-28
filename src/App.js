0e0c5f98cf2ff5c9a32a47b85b5372fea273f679
ApoloJ <mjlozanoh98@gmail.com>
2021-01-28 15:40:29 -0500

    search method added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
   componentDidMount() {
@@ -16,9 +17,18 @@ class App extends Component {
     );
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <input
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from '../card/card';
 
 const CardList = (props) => {
-    console.log(props);
     return ( <div className='card-list'>{props.monsters.map((monster) => (
         <Card key={monster.id} monster={monster}/>
           ))}</div> );
